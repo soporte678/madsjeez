404c8170b4937c880ccfdfe21666ef4ec6bc8fe7
Tu Nombre <tu-email@ejemplo.com>
2026-04-24 23:42:44 -0300

    Migrate to Prisma 7 PG Adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,19 @@
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL
-})
+// 1. Configuramos el Pool de conexiones nativo de Postgres
+const connectionString = `${process.env.DATABASE_URL}`
+const pool = new Pool({ connectionString })
+
+// 2. Creamos el adaptador de Prisma
+const adapter = new PrismaPg(pool)
+
+// 3. Inicializamos el cliente EXCLUSIVAMENTE con el adapter
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
