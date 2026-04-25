5522120c1ac5af3ed7991dc61501b702c8217c6f
Tu Nombre <tu-email@ejemplo.com>
2026-04-24 23:35:30 -0300

    Fix PrismaClient property a datasourceUrl

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,11 +5,7 @@ const globalForPrisma = globalThis as unknown as {
 }
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL
-    }
-  }
+  datasourceUrl: process.env.DATABASE_URL
 })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
